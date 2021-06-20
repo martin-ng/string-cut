2963e011fd88244f9401858ef22f647935cd1c68
Martin Ng <martinng1992@gmail.com>
2021-06-20 08:24:07 -0400

    setup initial server files

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+const PORT = 1337;
+
+app.listen(PORT, () => {
+  console.log(`Server is currently listening on port ${PORT}`);
+});
