64ad8c78c58fad760e7e7028e775ba47246438ce
Martin Ng <martinng1992@gmail.com>
2021-06-20 09:23:38 -0400

    refactored cut string and added tests

--- server/helper/cutString.js
+++ server/helper/cutString.js
@@ -1,8 +1,19 @@
-function cutString(inputStr) {
+/**
+ * This function was designed with an nthChar parameter for extensibility. This parameter has a default value of 3 for the purpose of the string cutting exercise.
+ * If a developer chooses to modify this parameter, the function can simply be invoked with the desired number value.
+ * @param {*} inputStr
+ * @param {*} nthChar
+ * @returns
+ */
+
+function cutString(inputStr, nthChar = 3) {
+  if (inputStr.length < nthChar) {
+    return `Your input needs to have at least ${nthChar} characters.`;
+  }
   let result = [];
 
-  for (let i = 2; i < inputStr.length; i += 2) {
-    result.push(string[i]);
+  for (let i = nthChar - 1; i < inputStr.length; i += nthChar) {
+    result.push(inputStr[i]);
   }
   return result.join("");
 }
--- server/tests/helper.test.js
+++ server/tests/helper.test.js
@@ -1,1 +1,31 @@
-const cutString = require("../server/helper/cutString");
+const cutString = require("../helper/cutString");
+
+describe(`stringCut with a default nthChar of every "3" characters`, () => {
+  let inputStr = "";
+  let message = "Your input needs to have at least 3 characters.";
+
+  test(`It should return a 'string' type`, () => {
+    inputStr = "iamyourlyftdriver";
+    expect(typeof cutString(inputStr)).toBe("string");
+  });
+
+  test(`It should return a message with the value of '3' if the input is empty`, () => {
+    inputStr = "";
+    expect(cutString(inputStr)).toBe(message);
+  });
+
+  test(`It should return a message if the input is less than '3' characters.`, () => {
+    inputStr = "ab";
+    expect(cutString(inputStr)).toBe(message);
+  });
+
+  test(`It should return 'muydv' if the input is 'iamyourlyftdriver'`, () => {
+    inputStr = "iamyourlyftdriver";
+    expect(cutString(inputStr)).toBe("muydv");
+  });
+
+  test(`It should return 'rn' if the input is 'martin'`, () => {
+    inputStr = "martin";
+    expect(cutString(inputStr)).toBe("rn");
+  });
+});
