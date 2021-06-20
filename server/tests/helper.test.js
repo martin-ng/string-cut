0275b45e665b81765664872ee31c618a23806ffa
Martin Ng <martinng1992@gmail.com>
2021-06-20 10:03:58 -0400

    added additional tests

--- server/tests/helper.test.js
+++ server/tests/helper.test.js
@@ -29,3 +29,32 @@ describe(`stringCut with a default nthChar of every "3" characters`, () => {
     expect(cutString(inputStr)).toBe("rn");
   });
 });
+
+describe(`stringCut with an nthChar value of every '2' characters`, () => {
+  let inputStr = "";
+  let nthChar = 2;
+  const message = "Your input needs to have at least 2 characters.";
+
+  test(`It should return a 'string' type`, () => {
+    expect(typeof cutString(inputStr, nthChar)).toBe("string");
+  });
+
+  test(`It should return a message with the value of '2' if the input is empty`, () => {
+    expect(cutString(inputStr, nthChar)).toBe(message);
+  });
+
+  test(`It should return a message if the input is less than 2 characters`, () => {
+    inputStr = "a";
+    expect(cutString(inputStr, nthChar)).toBe(message);
+  });
+
+  test(`It should return 'ayulfdie' if the input is 'iamyourlyftdriver'`, () => {
+    inputStr = "iamyourlyftdriver";
+    expect(cutString(inputStr, nthChar)).toBe("ayulfdie");
+  });
+
+  test(`It should return 'mri' if the input is 'martin'`, () => {
+    inputStr = "martin";
+    expect(cutString(inputStr, nthChar)).toBe("atn");
+  });
+});
