50a0c2da0cdde81ebee700a72ac50e199556101f
Martin Ng <martinng1992@gmail.com>
2021-06-20 21:16:07 -0400

    updated css

--- client/App.jsx
+++ client/App.jsx
@@ -6,6 +6,18 @@ const App = () => {
   return (
     <div>
       <Header />
+      <div className="container">
+        <div>
+          <h2>Hi World</h2>
+        </div>
+
+        <div>
+          <input></input>
+        </div>
+        <div>
+          <button class="btn">Cut!</button>
+        </div>
+      </div>
     </div>
   );
 };
