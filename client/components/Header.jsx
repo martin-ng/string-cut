f3dc744ab81e4de7e8a18c7350f7bceec4cbf073
Martin Ng <martinng1992@gmail.com>
2021-06-20 20:14:54 -0400

    added a header with css

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-// import "../css/app.css";
+import "../css/app.css";
 // import "./components.css";
 
 export const Header = () => {
   return (
-    <header>
+    <header className="header-container">
       <div className="logo">
         <h1>Cut String</h1>
       </div>
