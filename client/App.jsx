5884965cf12efe2bcca28bcdfb2cac4fc7e5f083
Martin Ng <martinng1992@gmail.com>
2021-06-20 22:30:19 -0400

    updated styling

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Header } from "./components/index";
-
 import "./css/app.css";
 
 const App = () => {
--- server/app.js
+++ server/app.js
@@ -10,11 +10,8 @@ const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
-// app.use(express.static(config.assets));
+app.use(express.static(config.assets));
 
-app.get("/hello", (req, res) => {
-  res.send("hello world");
-});
 app.use("/", routes);
 
 app.listen(config.PORT, () => {
